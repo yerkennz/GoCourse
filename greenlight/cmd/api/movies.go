906164a5265564deac1fe743d28d823210112cb6
Nursultan Yerkenov <yerken1656@gmail.com>
2023-09-30 19:47:23 +0600

    add 1 2 3 chapters

---
+++ greenlight/cmd/api/movies.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"greenlight.yerkennz.net/internal/data"
+	"net/http"
+	"time"
+)
+
+// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
+// return a plain-text placeholder response.
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
+// the interpolated "id" parameter from the current URL and include it in a placeholder
+// response.
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
