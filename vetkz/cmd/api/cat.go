c431022b0af27eb8e9fac9907a819158f4af8851
Nursultan Yerkenov <yerken1656@gmail.com>
2023-10-15 22:34:10 +0600

    add: cat post

--- vetkz/cmd/api/cat.go
+++ vetkz/cmd/api/cat.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 	"vetkz.yerkennz.net/internal/data"
 )
+
+func (app *application) createCatHandler(w http.ResponseWriter, r *http.Request) {
+
+	var input struct {
+		Title             string   `json:"title"`
+		Product           string   `json:"product"`
+		Packing           []string `json:"packing"`
+		Price             int64    `json:"price"`
+		TypePreparation   string   `json:"type_preparation"`
+		TypeFeed          string   `json:"type_feed"`
+		AgeCat            string   `json:"age_cat"`
+		SizeCat           string   `json:"size_cat"`
+		ActivityLevel     string   `json:"activity_level"`
+		Breed             string   `json:"breed"`
+		TypeProtection    string   `json:"type_protection"`
+		SpecialIndication string   `json:"special_indication"`
+		Taste             []string `json:"taste"`
+		TypeTool          string   `json:"type_tool"`
+		CountryOrigin     string   `json:"country_origin"`
+		Description       string   `json:"description"`
+		Quantity          int64    `json:"quantity"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+}
 
 func (app *application) showCatHandler(w http.ResponseWriter, r *http.Request) {
 
