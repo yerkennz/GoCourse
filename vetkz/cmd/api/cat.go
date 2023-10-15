09fa20071704f332fd190c5a948dc255b4e01d58
Nursultan Yerkenov <yerken1656@gmail.com>
2023-10-15 17:59:42 +0600

    add: helpers

--- vetkz/cmd/api/cat.go
+++ vetkz/cmd/api/cat.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"strconv"
 )
 
 func (app *application) showCatHandler(w http.ResponseWriter, r *http.Request) {
 
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
