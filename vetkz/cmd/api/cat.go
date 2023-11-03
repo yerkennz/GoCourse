48f5e9937d61ae723485a39adf3e2e8b10a03b63
Nursultan Yerkenov <yerken1656@gmail.com>
2023-11-03 14:22:32 +0600

    changes in table

--- vetkz/cmd/api/cat.go
+++ vetkz/cmd/api/cat.go
@@ -10,23 +10,15 @@ import (
 func (app *application) createCatHandler(w http.ResponseWriter, r *http.Request) {
 
 	var input struct {
-		Title             string   `json:"title"`
-		Product           string   `json:"product"`
-		Packing           []string `json:"packing"`
-		Price             int64    `json:"price"`
-		TypePreparation   string   `json:"type_preparation"`
-		TypeFeed          string   `json:"type_feed"`
-		AgeCat            string   `json:"age_cat"`
-		SizeCat           string   `json:"size_cat"`
-		ActivityLevel     string   `json:"activity_level"`
-		Breed             string   `json:"breed"`
-		TypeProtection    string   `json:"type_protection"`
-		SpecialIndication string   `json:"special_indication"`
-		Taste             []string `json:"taste"`
-		TypeTool          string   `json:"type_tool"`
-		CountryOrigin     string   `json:"country_origin"`
-		Description       string   `json:"description"`
-		Quantity          int64    `json:"quantity"`
+		Title         string `json:"title"`
+		Product       string `json:"product"`
+		Price         int64  `json:"price"`
+		AgeCat        string `json:"age_cat"`
+		SizeCat       string `json:"size_cat"`
+		Breed         string `json:"breed"`
+		CountryOrigin string `json:"country_origin"`
+		Description   string `json:"description"`
+		Quantity      int64  `json:"quantity"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -46,22 +38,17 @@ func (app *application) showCatHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	cat := data.Cat{
-		ID:                id,
-		CreatedAt:         time.Now(),
-		Title:             "СУХОЙ КОРМ",
-		Price:             44410,
-		Product:           "Corm",
-		TypeFeed:          "Сухой корм",
-		AgeCat:            "Взрослые (1 - 7 лет)",
-		SizeCat:           "Породы любого размера",
-		ActivityLevel:     "Нормальный",
-		Breed:             "Любая порода",
-		SpecialIndication: "Кастрация и стерилизация",
-		CountryOrigin:     "Россия",
-		Description:       "Void",
-		Quantity:          64,
-		Taste:             []string{"Кролик", "Лосось", "Индейка"},
-		Packing:           []string{"400 гр", "1,5 кг"},
+		ID:            id,
+		CreatedAt:     time.Now(),
+		Title:         "СУХОЙ КОРМ",
+		Price:         44410,
+		Product:       "Corm",
+		AgeCat:        "Взрослые (1 - 7 лет)",
+		SizeCat:       "Породы любого размера",
+		Breed:         "Любая порода",
+		CountryOrigin: "Россия",
+		Description:   "Void",
+		Quantity:      64,
 	}
 	err = app.writeJSON(w, http.StatusOK, envelope{"cat": cat}, nil)
 	if err != nil {
