d9fafaddd69b62d31df05558266627480d6f3e14
Nursultan Yerkenov <yerken1656@gmail.com>
2023-11-03 23:42:31 +0600

    add: crud(delete)

--- vetkz/cmd/api/routes.go
+++ vetkz/cmd/api/routes.go
@@ -15,6 +15,7 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/cat/:id", app.showCatHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/cat", app.createCatHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/cat/:id", app.updateCatHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteCatHandler)
 
 	return router
 }
