2e7f27c078eff5d74c0eda6417fe15779feedeba
Nursultan Yerkenov <yerken1656@gmail.com>
2023-11-14 20:50:22 +0600

    add: user makeEmail

--- vetkz/cmd/api/main.go
+++ vetkz/cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 	"vetkz.yerkennz.net/internal/data"
 	"vetkz.yerkennz.net/internal/jsonlog"
+	"vetkz.yerkennz.net/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -52,6 +61,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "7a099e857e713c", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "364e32ec7ce96d", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "VETKZ <no-reply@vetkz.yerkennz.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -72,6 +87,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 	err = app.serve()
 	if err != nil {
