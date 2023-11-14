4e87727726f780b5fc9bba9fe75592ba5874a5ef
Nursultan Yerkenov <yerken1656@gmail.com>
2023-11-14 16:21:06 +0600

    add: user table

--- vetkz/internal/data/models.go
+++ vetkz/internal/data/models.go
@@ -15,13 +15,15 @@ var (
 // Create a Models struct which wraps the MovieModel. We'll add other models to this,
 // like a UserModel and PermissionModel, as our build progresses.
 type Models struct {
-	Cats CatModel
+	Cats  CatModel
+	Users UserModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing
 // the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Cats: CatModel{DB: db},
+		Cats:  CatModel{DB: db},
+		Users: UserModel{DB: db},
 	}
 }
